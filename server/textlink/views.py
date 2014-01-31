b91dbe2a831bb8c5f6eac8b5eab033108ec45235
Noah Goldman <noahg34@gmail.com>
2014-01-31 15:43:39 -0500

    Added json helper for view functions

--- server/textlink/views.py
+++ server/textlink/views.py
@@ -3,12 +3,14 @@ from flask import request
 from textlink import app, Session
 from textlink.models import Number, Phone, List
 from textlink.Obj2JSON import jsonobj
+from textlink.helpers import API
 
 @app.route('/')
 def index():
     return "Hello World!"
 
 @app.route('/lists', methods=['POST'])
+@API
 def create_list():
     name = request.form.get('name')
     print name
