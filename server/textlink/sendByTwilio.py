5d67aa5012071485c07d73964d1ee6fc84c1287a
Noah Goldman <noahg34@gmail.com>
2014-01-30 22:00:01 -0500

    Removed sketchy stuff

--- server/textlink/sendByTwilio.py
+++ server/textlink/sendByTwilio.py
@@ -10,5 +10,3 @@ def sendSMS(sid, tok, sender, reciever, msg):
         to=reciever,   
         from_=sender) 
     print message.sid
-
-sendSMS("AC0955b5ae6e4e14861d9e1f61e7d0680f","ee7833a07032d4a600f47a12aa6af464", "+17324105138", "+19086702635", "Yo") 
